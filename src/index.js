fc32ece08c385ff804169057fa168159e4475b08
Ironipad64 <Ironipad64@gmail.com>
2021-02-16 14:39:23 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers/featureReducer';
+
 import 'bulma/css/bulma.css';
-import './styles.scss';
+import './styles.css';
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={reducer}>
+        <App />
+    </Provider>
+    , rootElement);
